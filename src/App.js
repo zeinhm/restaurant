3d3e68a20da78c6ffd7523d70ab890eda8b71b9c
Zein Hammad Marhabah <zein@tebengan.id>
2020-06-15 22:02:45 +0700

    fix viewport restrict trouble, meta tag value

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Home from 'pages/Home'
 import Order from 'pages/Order'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ViewportRestrict from 'vp-restrict-lib'
+import ViewportRestrict from './components/viewport/index'
 
 function App() {
   return (
     <Router>
-      <ViewportRestrict type="landscape"
+      <ViewportRestrict type="landscape" display={true}
         text="Please turn your smartphone to a portrait position for better visuals"
       />
       <div className="App">
